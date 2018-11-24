4af41f162da1580b73092625f6cf49fcdedf1e91
pedromassango <pedromassango.developer@gmail.com>
2018-11-25 00:30:01 +0100

    moved to androidx

--- app/src/main/java/com/pedromassango/crypto/MainActivity.kt
+++ app/src/main/java/com/pedromassango/crypto/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.pedromassango.crypto
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.pedromassango.crypto.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/pedromassango/crypto/ui/main/MainViewModel.kt
+++ app/src/main/java/com/pedromassango/crypto/ui/main/MainViewModel.kt
@@ -1,6 +1,6 @@
 package com.pedromassango.crypto.ui.main
 
-import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
