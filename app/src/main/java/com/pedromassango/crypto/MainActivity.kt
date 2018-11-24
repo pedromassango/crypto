23ef1f0b1bf6db8b1a2bfbaea37b33e3b8ec1099
pedromassango <pedromassango.developer@gmail.com>
2018-11-25 00:50:02 +0100

    added navigation component

--- app/src/main/java/com/pedromassango/crypto/MainActivity.kt
+++ app/src/main/java/com/pedromassango/crypto/MainActivity.kt
@@ -2,18 +2,21 @@ package com.pedromassango.crypto
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.pedromassango.crypto.ui.main.MainFragment
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :
+    AppCompatActivity(),
+    NavHost {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
     }
 
+    override fun getNavController(): NavController =
+        Navigation.findNavController(this, nav_host_fragment.id)
 }
