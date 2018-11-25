9e6f57718cf849c83328b6b90e60accfb0e4aeae
pedromassango <pedromassango.developer@gmail.com>
2018-11-25 01:46:54 +0100

    navigation component implementation done

--- app/src/main/java/com/pedromassango/crypto/MainActivity.kt
+++ app/src/main/java/com/pedromassango/crypto/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity :
@@ -15,6 +16,12 @@ class MainActivity :
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        // setting up navigation component along with  BottomNavigationView
+        NavigationUI.setupWithNavController(bottom_navigation, navController)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            navController.navigate(it.itemId)
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 
     override fun getNavController(): NavController =
