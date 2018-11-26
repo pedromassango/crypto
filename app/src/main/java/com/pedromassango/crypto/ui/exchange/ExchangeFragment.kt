2b0e05dac52ca530da69f629704914ff7f9b5b40
pedromassango <pedromassango.developer@gmail.com>
2018-11-27 00:11:36 +0100

    small updates

--- app/src/main/java/com/pedromassango/crypto/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/pedromassango/crypto/ui/exchange/ExchangeFragment.kt
@@ -19,7 +19,6 @@ class ExchangeFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return with(inflater.inflate(R.layout.exchange_fragment, container, false)){
-
             btn_exchange.setOnClickListener {  startExchanging() }
             this
         }
