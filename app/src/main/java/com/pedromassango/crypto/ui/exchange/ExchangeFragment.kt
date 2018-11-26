a38b6d53fc27bab576525b5ecf85f59f3e831eaa
pedromassango <pedromassango.developer@gmail.com>
2018-11-26 22:52:34 +0100

    handling with exchange viewModel

--- app/src/main/java/com/pedromassango/crypto/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/pedromassango/crypto/ui/exchange/ExchangeFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
 import com.pedromassango.crypto.R
 import kotlinx.android.synthetic.main.exchange_fragment.*
 import kotlinx.android.synthetic.main.exchange_fragment.view.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class ExchangeFragment: Fragment() {
+
+    private val exchangeViewModel: ExchangeViewModel by viewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return with(inflater.inflate(R.layout.exchange_fragment, container, false)){
@@ -19,8 +24,29 @@ class ExchangeFragment: Fragment() {
         }
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        exchangeViewModel.error.observe(this, Observer {
+            Snackbar.make(view!!, it, Snackbar.LENGTH_LONG).show()
+        })
+        exchangeViewModel.exchangeResult.observe(this, Observer {
+            tv_exchange_result.text = it.toString()
+
+            edt_exchange.isEnabled = true
+            btn_exchange.isEnabled = true
+            progress_bar.visibility = View.GONE
+        })
+    }
+
     private fun startExchanging() {
         val valueToExchange = edt_exchange.text.toString()
         val currency = spinner_currency.selectedItem.toString()
+
+        edt_exchange.isEnabled = false
+        btn_exchange.isEnabled = false
+        progress_bar.visibility = View.VISIBLE
+
+        exchangeViewModel.onExchangeClicked(currency, valueToExchange)
     }
 }
--- app/src/main/java/com/pedromassango/crypto/ui/exchange/ExchangeViewModel.kt
+++ app/src/main/java/com/pedromassango/crypto/ui/exchange/ExchangeViewModel.kt
@@ -1,6 +1,15 @@
 package com.pedromassango.crypto.ui.exchange
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ExchangeViewModel: ViewModel() {
+
+    val error = MutableLiveData<String>()
+    val exchangeResult = MutableLiveData<Long>()
+
+    fun onExchangeClicked(currency: String,
+                          valueToExchange: String) {
+
+    }
 }
