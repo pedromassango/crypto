d63f725417fb2082a4fb3a0277829db56010cc04
pedromassango <pedromassango.developer@gmail.com>
2018-11-26 23:53:27 +0100

    added read and connect timeout

--- app/src/main/java/com/pedromassango/crypto/data/BlockchainApiClient.kt
+++ app/src/main/java/com/pedromassango/crypto/data/BlockchainApiClient.kt
@@ -2,18 +2,26 @@ package com.pedromassango.crypto.data
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.POST
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 object BlockchainApiClient{
+
+    private val okHttClient = OkHttpClient.Builder()
+        .readTimeout(20, TimeUnit.SECONDS)
+        .connectTimeout(200, TimeUnit.SECONDS)
+        .build()
 
      private val builder = Retrofit.Builder()
          .baseUrl("https://blockchain.info/")
          .addConverterFactory(GsonConverterFactory.create())
          .addCallAdapterFactory(CoroutineCallAdapterFactory())
+         .client(okHttClient)
          .build()
 
     val apiService = builder.create(BlockchainService::class.java)
@@ -25,5 +33,5 @@ interface BlockchainService{
     fun exchangeToBtc(
         @Query("currency") currency: String,
         @Query("value") valueToConvert: String
-    ): Deferred<Response<Long>>
+    ): Deferred<Response<Double>>
 }
