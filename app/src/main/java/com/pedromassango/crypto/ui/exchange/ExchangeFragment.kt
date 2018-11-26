807cd82a871051c33eb774abf4624899304f9dfd
pedromassango <pedromassango.developer@gmail.com>
2018-11-26 22:28:21 +0100

    UI updated to just convert any currency to BTC

--- app/src/main/java/com/pedromassango/crypto/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/pedromassango/crypto/ui/exchange/ExchangeFragment.kt
@@ -6,12 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.pedromassango.crypto.R
+import kotlinx.android.synthetic.main.exchange_fragment.*
+import kotlinx.android.synthetic.main.exchange_fragment.view.*
 
 class ExchangeFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return with(inflater.inflate(R.layout.exchange_fragment, container, false)){
+
+            btn_exchange.setOnClickListener {  startExchanging() }
             this
         }
+    }
+
+    private fun startExchanging() {
+        val valueToExchange = edt_exchange.text.toString()
+        val currency = spinner_currency.selectedItem.toString()
     }
 }
